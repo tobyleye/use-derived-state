930e3334a738dd49328d051f1d9e6c56eb2ef7b4
Denis Homolik <alfonz@homolik.cz>
2021-02-12 11:09:59 +0100

    Update state immediately after initialState change

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-import * as React from "react";
+import { useRef, useState, useEffect } from "react";
 
-export default function useDerivedState(value) {
-  const [state, setState] = React.useState(value);
-  React.useEffect(() => {
-    setState(value);
-  }, [value]);
-  return [state, setState];
-}
+const useDerivedState = (initialState, deps = initialState) => {
+  const prevDeps = useRef();
+
+  const [state, set] = useState(initialState);
+
+  useEffect(() => set(initialState), deps);
+
+  const returnValue = prevDeps.current === undefined || state === initialState || areHookInputsEqual(deps, prevDeps.current)
+    ? state
+    : evaluateState(initialState);
+  prevDeps.current = deps;
+
+  return [returnValue, set];
+};
+
+export default useDerivedState;
+
+const areHookInputsEqual = (nextDeps, prevDeps) => {
+  if (nextDeps.length !== prevDeps.length) {
+    return false;
+  }
+
+  for (let i = 0; i < prevDeps.length && i < nextDeps.length; i++) {
+    if (Object.is(nextDeps[i], prevDeps[i])) {
+      continue;
+    }
+
+    return false;
+  }
+
+  return true;
+};
+
+const evaluateState = s => typeof s === "function" ? s() : s;
