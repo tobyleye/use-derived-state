41c6d91b625eadfa3559326eabd7444b40276c84
Denis Homolik <alfonz@homolik.cz>
2021-02-13 14:27:15 +0100

    Optimize hook to make only single re-render

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 
 const useDerivedState = (initialState, deps = initialState) => {
   const prevDeps = useRef();
 
   const [state, set] = useState(initialState);
 
-  useEffect(() => set(initialState), deps);
+  const setValue = useCallback(s => {
+    // Reset deps to allow returning local state
+    prevDeps.current = undefined;
+    set(s);
+  }, []);
 
-  const returnValue = prevDeps.current === undefined || state === initialState || areHookInputsEqual(deps, prevDeps.current)
-    ? state
-    : evaluateState(initialState);
-  prevDeps.current = deps;
+  const shouldKeepState = prevDeps.current === undefined || state === initialState || areHookInputsEqual(deps, prevDeps.current);
 
-  return [returnValue, set];
+  const returnValue = shouldKeepState ? state : evaluateState(initialState);
+
+  // Update deps if local state is returned
+  if (shouldKeepState) {
+    prevDeps.current = deps;
+  }
+
+  return [returnValue, setValue];
 };
 
 export default useDerivedState;
 
+// Compare if hook inputs are equal, taken from and modified: 
+// https://github.com/facebook/react/blob/1a7472624661270008011fd77f097d71e6249de9/packages/react-reconciler/src/ReactFiberHooks.new.js#L304
 const areHookInputsEqual = (nextDeps, prevDeps) => {
   if (nextDeps.length !== prevDeps.length) {
     return false;
